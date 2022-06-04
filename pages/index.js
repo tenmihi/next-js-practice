2ce62fe07be3a3195c99d5eefd34a8f6eda8dc01
tenmihi <tenmihi@gmail.com>
2022-06-04 11:59:20 +0900

    feat(pages/index.js): postリストを調整

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import Layout, { siteTitle } from '../components/layout'
 import { getSortedPostData } from '../lib/posts'
 import utilStyles from '../styles/util.module.css'
@@ -29,11 +30,13 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({id ,date ,title}) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
